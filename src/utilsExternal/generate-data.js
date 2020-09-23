226a3c9b94bfe5e608c3febb8e4adf4076743bf0
mikproto <mojpo4tovik@gmail.com>
2020-09-23 21:49:38 +0300

    added row class

--- src/utilsExternal/generate-data.js
+++ src/utilsExternal/generate-data.js
@@ -11,6 +11,9 @@ export function generateFakeDataObject(rowsNumber, colsNumber) {
             result[row] = {};
         }
         result[row][col] = row + ':' + col;
+        if (row === 3) {
+            result[row]['selected'] = 'green';
+        }
         if (!columns[col]) {
             columns[col] = {
                 name: generateHeader(col),
