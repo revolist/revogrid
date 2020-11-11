2548bec9b0713e04dc3ac92c06a7f0f24b830d84
revolist <maksim.k@revolist.eu>
2020-11-11 10:09:38 -0500

    plugin support

--- src/components/revo-grid/revo-grid.tsx
+++ src/components/revo-grid/revo-grid.tsx
@@ -10,7 +10,6 @@ import ViewportProvider from '../../services/viewport.provider';
 import {Edition, Selection, RevoGrid, ThemeSpace} from '../../interfaces';
 import ThemeService from '../../themeManager/themeService';
 import { timeout } from '../../utils/utils';
-import SelectColumnType from '../../plugins/select/select';
 
 
 type ColumnStores = {
@@ -88,9 +87,7 @@ export class RevoGridComponent {
    *  Every type represent multiple column properties
    *  Types will be merged but can be replaced with column properties
    */
-  @Prop() columnTypes: {[name: string]: RevoGrid.ColumnType} = {
-    'select': new SelectColumnType()
-  };
+  @Prop() columnTypes: {[name: string]: RevoGrid.ColumnType};
 
 
   /** Theme name */
