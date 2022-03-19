6968096baec9b66047b573b2701328e2c2d8b060
islarky88 <islarky88@gmail.com>
2022-03-19 11:50:03 +0800

    add trash  button

--- src/plugins/filter/filter.button.tsx
+++ src/plugins/filter/filter.button.tsx
@@ -27,6 +27,16 @@ export const FilterButton = ({ column }: Props) => {
   );
 };
 
+export const TrashButton = () => {
+  return (
+    <span>
+      <svg style={{ width: '18px', height: '18px' }} viewBox="0 0 24 24">
+        <path fill="currentColor" d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" />
+      </svg>
+    </span>
+  );
+};
+
 export function isFilterBtn(e: HTMLElement) {
   if (e.classList.contains(FILTER_BUTTON_CLASS)) {
     return true;
