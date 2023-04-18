5d69c1433d97adb8e09d2e1eee4b87d6d24f4bc2
maks <mojpo4tovik@gmail.com>
2023-04-18 13:11:51 +0100

    events correction for sample

--- src/utils/resizeObserver.ts
+++ src/utils/resizeObserver.ts
@@ -1,6 +1,6 @@
 export async function resizeObserver() {
   if (!('ResizeObserver' in window)) {
     const module = await import('@juggle/resize-observer');
-    window.ResizeObserver = (module.ResizeObserver as unknown) as typeof ResizeObserver;
+    (window as Window & typeof globalThis).ResizeObserver = (module.ResizeObserver as unknown) as typeof ResizeObserver;
   }
 }
