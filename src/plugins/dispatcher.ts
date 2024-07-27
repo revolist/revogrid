f56bf50e3d2048c8d7f3081240be2216cdbe01d4
maks <mojpo4tovik@gmail.com>
2024-07-27 16:12:16 +0100

    fix: typings for dispatcher

--- src/plugins/dispatcher.ts
+++ src/plugins/dispatcher.ts
@@ -1,28 +1,46 @@
 /**
- * Dispatch custom event to element
+ * Dispatches a custom event to a specified target element.
+ *
+ * @param target - The target element to dispatch the event to.
+ * @param eventName - The name of the custom event.
+ * @param detail - Optional. The detail of the custom event.
+ * @returns The custom event that was dispatched.
  */
 export function dispatch<DispatchDetail = any>(
   target: MouseEvent['target'],
   eventName: string,
   detail?: DispatchDetail,
-): CustomEvent {
-  const event = new CustomEvent(eventName, {
+) {
+  // Create a new CustomEvent with the specified event name and detail.
+  const event = new CustomEvent<DispatchDetail>(eventName, {
     detail,
-    cancelable: true,
-    bubbles: true,
+    cancelable: true, // Indicates whether the event can be canceled.
+    bubbles: true, // Indicates whether the event bubbles up through the DOM.
   });
+
+  // Dispatch the event on the target element.
   target?.dispatchEvent(event);
+
+  // Return the custom event that was dispatched.
   return event;
 }
 
 /**
- * Dispatch custom event by event object and prevent default
+ * Dispatches a custom event based on an existing event object and prevents the default behavior of the original event.
+ *
+ * @param e - The original event object containing the target and preventDefault method.
+ * @param eventName - The name of the custom event.
+ * @param detail - Optional. The detail of the custom event.
+ * @returns The custom event that was dispatched.
  */
 export function dispatchByEvent<DispatchDetail = any>(
-  e: Pick<MouseEvent, 'target' | 'preventDefault'>,
-  eventName: string,
-  detail?: DispatchDetail,
+  e: Pick<MouseEvent, 'target' | 'preventDefault'>, // The original event object containing the target and preventDefault method.
+  eventName: string, // The name of the custom event.
+  detail?: DispatchDetail, // Optional. The detail of the custom event.
 ): CustomEvent {
+  // Prevent the default behavior of the original event.
   e.preventDefault();
-  return dispatch(e.target, eventName, detail);
+
+  // Dispatch the custom event to the target element specified in the original event object.
+  return dispatch<DispatchDetail>(e.target, eventName, detail);
 }
