477507f867ff98f395e0119897545945e222b246
Maksim Kudrytski <kudrytskimaksim@gmail.com>
2024-08-14 14:11:36 +0000

    feat: improved keyboard event pass original support

--- src/components/overlay/revogr-overlay-selection.tsx
+++ src/components/overlay/revogr-overlay-selection.tsx
@@ -262,13 +262,13 @@ export class OverlaySelection {
    * Call preventDefault().
    */
   @Event({ eventName: 'beforekeydown' })
-  beforeKeyDown: EventEmitter<KeyboardEvent & EventData>;
+  beforeKeyDown: EventEmitter<{ original: KeyboardEvent } & EventData>;
   /**
    * Before key down event proxy, used to prevent key down trigger.
    * If you have some custom behaviour event, use this event to check if it wasn't processed by internal logic.
    * Call preventDefault().
    */
-  @Event({ eventName: 'beforekeyup' }) beforeKeyUp: EventEmitter<KeyboardEvent & EventData>;
+  @Event({ eventName: 'beforekeyup' }) beforeKeyUp: EventEmitter<{ original: KeyboardEvent } & EventData>;
   /**
    * Runs before cell save.
    * Can be used to override or cancel original save.
@@ -331,7 +331,7 @@ export class OverlaySelection {
    */
   @Listen('keyup', { target: 'document' }) onKeyUp(e: KeyboardEvent) {
     // Emit before key up event.
-    this.beforeKeyUp.emit({ ...e, ...this.getData() });
+    this.beforeKeyUp.emit({ original: e, ...this.getData() });
   }
 
   /**
@@ -340,7 +340,7 @@ export class OverlaySelection {
    */
   @Listen('keydown', { target: 'document' }) onKeyDown(e: KeyboardEvent) {
     // Emit before key down event and check if default prevention is set.
-    const proxy = this.beforeKeyDown.emit({ ...e, ...this.getData() });
+    const proxy = this.beforeKeyDown.emit({ original: e, ...this.getData() });
     if (e.defaultPrevented || proxy.defaultPrevented) {
       return;
     }
