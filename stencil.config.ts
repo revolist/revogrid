8b4eba616d5ddd4a56e6d549f319eacdc05709e1
maks <maks@makss-MacBook-Pro.local>
2025-03-05 23:14:12 +0000

    feat: theme in vars

--- stencil.config.ts
+++ stencil.config.ts
@@ -39,7 +39,7 @@ export const config: Config = {
   hashFileNames: false,
   autoprefixCss: false,
   minifyCss: true,
-  minifyJs: true,
+  minifyJs: false, // Keep VNode unminified for inner properties access
   preamble: 'Built by Revolist OU ❤️',
   hashedFileNameLength: 8,
   invisiblePrehydration: false,
